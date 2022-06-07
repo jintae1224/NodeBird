2fe76eab5dd99b3af87068ddd9d75999c2d732b0
JinTae <kab7210@naver.com>
2022-06-07 19:01:08 +0900

    Head 수정

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,9 +1,16 @@
 import PropTypes from 'prop-types'
 import 'antd/dist/antd.css'
+import Head from 'next/head'
 
 const App = ({Component}) => {
     return(
-        <Component />
+        <>
+            <Head>
+                <meta charSet='utf-8' />
+                <title>NodeBird</title>
+            </Head>
+            <Component />
+        </>
     )
 }
 
