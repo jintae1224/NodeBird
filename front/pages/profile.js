81785f0eae5c1fb5e87c6814601f71f5d3b4074e
JinTae <kab7210@naver.com>
2022-06-13 19:26:41 +0900

    Profile

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,16 +1,22 @@
 import AppLayout from "../components/AppLayout";
 import Head from "next/head"
+import NickNameEditForm from "../components/NicknamesEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
+
+    const followerList = [{nickname: "제로초"}, {nickname: "헤헤"}, {nickname: "헤헤헿"}]
+    const followingList = [{nickname: "노드버드"}, {nickname: "아아"}, {nickname: "호호"}]
+
     return(
         <>
             <Head>
                 <title>내 프로필 | NodeBird</title>
             </Head>
             <AppLayout>
-                <div>
-                    내 프로필
-                </div>
+                <NickNameEditForm />
+                <FollowList header="팔로잉 목록" data={followingList}/>
+                <FollowList header="팔로워 목록" data={followerList}/>
             </AppLayout>
         </>
     );
