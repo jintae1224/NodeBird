7fc3f0f70db847800a8e9eed80b86d0c6e8c50fd
jintae-1224 <kab7210@naver.com>
2022-10-20 00:59:38 +0900

    post.js Dummy

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,4 +1,42 @@
-export const initialState = { mainPosts: [] };
+export const initialState = {
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "제로초",
+      },
+      content: "첫 번째 게시글 #해시태그 #익스프레스",
+      images: [
+        {
+          src: "https://sunstat.com/wp-content/uploads/2019/01/%EC%8A%AC%EB%9D%BC%EC%9D%B4%EB%93%9C-%EB%B0%B0%EA%B2%BD%EC%9D%B4%EB%AF%B8%EC%A7%80.png",
+        },
+        {
+          src: "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
+        },
+        {
+          src: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=http%3A%2F%2Fcfile6.uf.tistory.com%2Fimage%2F257E0B3655214FFD03A829",
+        },
+      ],
+      Comment: [
+        {
+          User: {
+            nickname: "nero",
+          },
+          content: "와우",
+        },
+        {
+          User: {
+            nickname: "hero",
+          },
+          content: "굿",
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
