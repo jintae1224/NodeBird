c6917f874855cb65a04609d90779323f74c9c5ef
jintae-1224 <kab7210@naver.com>
2022-10-20 01:03:26 +0900

    게시글 추가 action Dummy

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -38,8 +38,30 @@ export const initialState = {
   postAdded: false,
 };
 
+const ADD_POST = "ADD_POST";
+export const addPost = {
+  type: ADD_POST,
+};
+const dummyPost = {
+  id: 2,
+  content: "더미데이터",
+  User: {
+    id: 1,
+    nickname: "제로초",
+  },
+  Images: [],
+  Comments: [],
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
+
     default:
       return state;
   }
