8e51b3ea5df9b87a1cf70cf57a79f5c004b07c2b
JinTae <kab7210@naver.com>
2022-10-28 16:08:42 +0900

    MiddleWare 추가

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,11 +1,12 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { ThunkMiddleware } from "redux-thunk";
 
 import reducer from "../reducers";
 
 const configureStore = () => {
-  const middlewares = [];
+  const middlewares = [ThunkMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
