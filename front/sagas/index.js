48dd3fd126a006a03efb03056e72864db5bcb519
jintae-1224 <kab7210@naver.com>
2022-10-30 17:20:25 +0900

    logout, addpost 추가

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -12,11 +12,49 @@ function* logIn() {
       type: "LOG_IN_SUCCESS",
       data: result.data,
     });
-  } catch(err) {
+  } catch (err) {
     yield put({
-        type: "LOG_IN_FAILURE",
-        data: err.response.data
-    })
+      type: "LOG_IN_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function logOutApi() {
+  return axios.post("/api/logout");
+}
+
+function* logOut() {
+  try {
+    const result = yield call(logOutApi);
+    yield put({
+      type: "LOG_OUT_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "LOG_OUT_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function addPostApi() {
+  return axios.post("/api/post");
+}
+
+function* addPost() {
+  try {
+    const result = yield call(addPostApi);
+    yield put({
+      type: "ADD_POST_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "ADD_POST_FAILURE",
+      data: err.response.data,
+    });
   }
 }
 
@@ -24,10 +62,10 @@ function* watchLogin() {
   yield take("LOG_IN_REQUEST", logIn);
 }
 function* watchLogOut() {
-  yield take("LOG_OUT_REQUEST");
+  yield take("LOG_OUT_REQUEST", logOut);
 }
 function* watchAddPost() {
-  yield take("ADD_POST_REQUEST");
+  yield take("ADD_POST_REQUEST", addPost);
 }
 
 export default function* rootSaga() {
