bbe81fab8835e993621c8728e65e359efab5fd8a
jintae-1224 <kab7210@naver.com>
2022-11-06 22:38:22 +0900

    무한 스크롤

--- front/pages/index.js
+++ front/pages/index.js
@@ -7,30 +7,40 @@ import { LOAD_POSTS_REQUEST } from "../reducers/post";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const { me } = useSelector((state) => state.user);
+  const { mainPosts, hasMorePost, loadPostLoading } = useSelector(
+    (state) => state.post
+  );
+
   useEffect(() => {
     dispatch({
       type: LOAD_POSTS_REQUEST,
     });
   }, []);
 
-  // useEffect(() => {
-  //   function onScroll() {
-  //     console.log(
-  //       window.scrollY,
-  //       document.documentElement.clientHeight,
-  //       document.documentElement.scrollHeight
-  //     );
-  //   }
-  //   window.addEventListener("scroll", onscroll);
-  //   return () => {
-  //     window.removeEventListener("scroll", onscroll);
-  //   };
-  // }, []);
-
-  const { me } = useSelector((state) => state.user);
-  const { mainPosts } = useSelector((state) => state.post);
-
-  console.log("dddddd", mainPosts);
+  useEffect(() => {
+    function onScroll() {
+      console.log(
+        window.scrollY,
+        document.documentElement.clientHeight,
+        document.documentElement.scrollHeight
+      );
+      if (
+        window.scrollY + document.documentElement.clientHeight >
+        document.documentElement.scrollHeight - 300
+      ) {
+        if (hasMorePost && !loadPostLoading) {
+          dispatch({
+            type: LOAD_POSTS_REQUEST,
+          });
+        }
+      }
+    }
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [hasMorePost, loadPostLoading]);
 
   return (
     <AppLayout>
